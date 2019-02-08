93e9bbbf062e4220db28a569ff43d985022afa8f
alumno <alumno>
2019-02-08 09:36:07 -0300

    guia de git

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,16 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var a;
+	var b;
+
+	a=document.getElementById('numeroUno').value;
+	b=document.getElementById('numeroDos').value,
+
+	a=parseInt(a);
+	b=parseInt(b);
+
+	alert(a+b);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,49 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
+	var a;
+	var b;
+
+	a=document.getElementById('').value
+	b=document.getElementById('').value
+
+	a=parseInt(a);
+	b=parseInt(b);
+
+	alert(a+b);
 	
 }
 
 function restar()
 {
+	var a;
+	var b;
+
+	a=document.getElementById('').value;
+	b=document.getElementById('').value;
 	
+	alert(a-b);
 }
 
 function multiplicar()
 { 
-	
+	var a;
+	var b;
+
+	a=document.getElementById('').value;
+	b=document.getElementById('').value;
+
+	alert(a*b);
 }
 
 function dividir()
 {
-	
+	var a;
+	var b;
+
+	a=document.getElementById('').value;
+	b=document.getElementById('').value;
+
+	alert(a/b);
 }
 
