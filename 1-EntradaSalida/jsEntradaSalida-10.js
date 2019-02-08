7ee98f2ed2a0a5de5414ffc7a3023157b9fb5790
alumno <alumno>
2019-02-08 11:47:40 -0300

    termine el 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,15 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe1;
+	var descuento; 
+
+	importe1=document.getElementById('importe').value;
+
+	importe1=parseInt(importe1);
+
+	descuento=importe1*25/100;
+
+	document.getElementById('resultado').value=importe1-descuento;
+
 }
