ecd7f977f27ba7b9cdba897b241c9fadfadbc65c
alumno <alumno>
2019-02-08 12:00:31 -0300

    Parcial y Recuperatorio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var peri;
+
+	ancho=prompt("Escriba el ancho del rectangulo");
+	largo=prompt("Escriba el largo del rectangulo");
+
+	var peri=ancho*2+largo*2;
+
+	alert("El perimetro es "+peri);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var peri;
 
+	ancho=prompt("Escriba el ancho del rectangulo");
+	largo=prompt("Escriba el largo del rectangulo");
+
+	var peri=ancho*2+largo*2;
+
+	alert("El perimetro es "+peri);
 }
