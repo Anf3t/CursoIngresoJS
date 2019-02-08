3191dce13988cab6a893d98c406796f821eb6e29
alumno <alumno>
2019-02-08 12:18:56 -0300

    Termine parcial y recuperatorio 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var final;
+
+	precio=prompt("Precio ");
+	descuento=prompt("Descuento de ");
+
+	final=precio*descuento/100;
+
+	document.getElementById('elPrecioFinal').value=precio-final;
+
 
 }
