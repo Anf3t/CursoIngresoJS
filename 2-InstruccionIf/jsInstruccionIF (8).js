cf74d10b7bc58eadb7c63e497db51ad3d889cefe
alumno <alumno>
2019-02-14 12:09:38 -0300

    Arregle ejercicio 8 if

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -7,9 +7,6 @@ function mostrar()
 	edad=document.getElementById('edad').value;
 	estado=document.getElementById('estadoCivil').value;
 
-	if(edad<=18&&estado!="Soltero"){
-
-	}
 	if(edad>=18&&estado=="Soltero"){
 		alert("Es soltero y no es menor.")
 	}
