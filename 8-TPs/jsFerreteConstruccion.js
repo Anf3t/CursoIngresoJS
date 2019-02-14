2be9ac6ece4f312deb27d564597b727ec8e4aedc
unknown <diwoovrgs@gmail.com>
2019-02-13 23:49:34 -0200

    Tp 2 por la mitad, radio no da resultado y Tp1 no sale la suma

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,61 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largor;
+	var anchor;
+	var rectangulo;
+	var alambre1;
 
+
+	largor=document.getElementById('Largo').value;
+	anchor=document.getElementById('Ancho').value;
+
+	largor=parseInt(largor);
+	anchor=parseInt(anchor);
+
+	rectangulo=largor*2+anchor*2;
+
+	alambre1=rectangulo*3;
+
+	alert("Se necesita "+alambre1+" metros de alambre.");
 }
 function Circulo () 
 {
+	var radior;
+	var diametro;
+	var perimetro;
+	var alambre2;
+
+	raidor=document.getElementById('Radio').value;
+
+	radior=parseInt(radior);
 	
+	diametro=radior+radior;
+	
+	perimetro=diametro*3.1416;
+
+	alambre2=perimetro*3;
+
+	alert(alambre2);
 }
 function Materiales () 
 {
+	var ancho2;
+	var alto2;
+	var area;
+	var cemento;
+	var cal;
+	var material;
+
+	ancho2=document.getElementById('Ancho').value;
+	alto2=document.getElementById('Largo').value;
+
+	area=ancho2*alto2;
+
+	cemento=area*2;
+
+	cal=area*3;
+
+	alert("Se necesitan "+cemento+" bolsas de cemento y "+cal+" de cal");
 	
 }
