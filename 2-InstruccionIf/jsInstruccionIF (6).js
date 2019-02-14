2827344a328725b9e7f702215e7a758eb75fa891
alumno <alumno>
2019-02-14 10:02:05 -0300

    Ejercicio 6 if

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var años;
 
-
+años=document.getElementById('edad').value;
+	if(años>=18){
+		alert("Usted es mayor de edad")
+	}
+	if(años==13||años==14||años==15||años==16||años==17){
+		alert("Usted es adolescente")
+	}
+	
+	if(años<=13){
+		alert("Usted es menor de edad")
+	}
 
 
 }//FIN DE LA FUNCIÓN
