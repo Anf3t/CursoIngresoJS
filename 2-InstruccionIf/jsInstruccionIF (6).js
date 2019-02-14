287195a5b8ce9d88a4a26af43bb585492b0e9ae5
alumno <alumno>
2019-02-14 11:31:53 -0300

    Arregle ejercicio 6 if

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -4,15 +4,16 @@ function mostrar()
 var años;
 
 años=document.getElementById('edad').value;
-	if(años>=18){
-		alert("Usted es mayor de edad")
-	}
-	if(años==13||años==14||años==15||años==16||años==17){
-		alert("Usted es adolescente")
-	}
 	
 	if(años<=13){
 		alert("Usted es menor de edad")
+	}else{
+		if(años<17){
+			alert("Usted es adolescente")
+		}
+		else{
+			alert("Usted es mayor")
+		}
 	}
 
 
