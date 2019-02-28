7696ad35e2de59d3739b2c32a0d02dcf9567021c
alumno <alumno>
2019-02-28 09:50:19 -0300

    swithc ej 5

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,11 +1,26 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+var laHora
+var msj
 
+ laHora = document.getElementById('hora').value;
+
+ switch(laHora){
+ 	case"7":
+ 	case"8":
+ 	case"9":
+ 	case"10":
+ 	case"11":
+ 		msj="Es de mañana";
+ 		break;
+ 	default:
+ 		msj="No es de mañana";	
+ 		break;
+ }	
 //alert (laHora);
 	
-	
+	alert(msj);
 
 
 
