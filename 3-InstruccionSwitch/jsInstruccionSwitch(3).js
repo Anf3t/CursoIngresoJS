24bde94225539b3377ff1e68540fc1e162f67faf
alumno <alumno>
2019-02-28 09:37:01 -0300

    Swicht ej 3

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,10 +1,23 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño
+var msj
 
+mesDelAño = document.getElementById('mes').value;
+
+switch(mesDelAño){
+
+case"Febrero":
+	msj="Este mes no tiene mas de 29 dias";
+	break;
+
+default:
+	msj="Este mes tiene 30 o mas dias";
+	break;
+}
 //alert (mesDelAño);
-	
+	alert(msj);
 	
 
 
